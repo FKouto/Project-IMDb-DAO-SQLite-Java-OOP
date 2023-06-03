6e9baf748c3f4495ef67d021d67d15a1f8096c7d
KoutoDev <126117449+KoutoDev@users.noreply.github.com>
2023-06-03 00:21:43 +0000

    método update implementado e testado

--- Main.java
+++ Main.java
@@ -5,21 +5,45 @@ class Main {
 
     FilmeDAO filmeDAO = new FilmeDAO();
 
-    //Quero consultar o banco trazendo todos os filmes..
+    // Quero consultar o banco trazendo todos os filmes..
     ArrayList<Filme> filmeLista = filmeDAO.getAll();
-    for (Filme filme: filmeLista) {
+    for (Filme filme : filmeLista) {
       System.out.println(filme);
     }
 
-    //Como fica para inserir filme agora..
-    Filme novoFilme = new Filme(0, "The Flash", (short) 2023);
-    filmeDAO.add(novoFilme);
+    // Como fica para inserir filme agora..
+    // Filme novoFilme = new Filme(0, "A Fuga das Galinhas", (short) 2000);
+    // filmeDAO.add(novoFilme);
 
-    //Quero consultar o banco trazendo todos os filmes..
+    // Quero consultar o banco trazendo todos os filmes..
     filmeLista = filmeDAO.getAll();
-    for (Filme filme: filmeLista) {
+    for (Filme filme : filmeLista) {
       System.out.println(filme);
-    }    
+    }
+
+    // Podemos consultar pontualmente qualquer filme..
+    // Trazer Braddock..
+    System.out.println("Trazer o filme Braddock...............");
+
+    Filme braddock = filmeDAO.getById(11);
+    System.out.println(braddock);
+
+    System.out.println("Trazer o filme 'Fuga' que está repetido para alteração...............");
+
+    Filme repetido = filmeDAO.getById(10);
+    System.out.println(repetido);
+
+    System.out.println("Trocando o filme 14 para Fuga2....");
     
+    repetido.setTitulo("A Fuga das Galinhas 2: A Missão");
+    repetido.setAno((short) 2024);
+    filmeDAO.update(repetido);
+
+    System.out.println("Vamos ver se alterou?");
+
+    filmeLista = filmeDAO.getAll();
+    for (Filme filme : filmeLista) {
+      System.out.println(filme);
+    }
   }
 }
